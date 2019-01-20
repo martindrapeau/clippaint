e91cbc23a4931ec1cb249316484c74a67615e69e
martindrapeau <martindrapeau@gmail.com>
2019-01-20 06:35:06 -0500

    Canvas resize now possible, just like in Paint.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 
   var $content = $('.content');
-  var $canvas = $('canvas').hide();
-  var canvas = $canvas[0];
-  var ctx = canvas.getContext("2d");
-  var draw = SVG('drawing').size(0, 0);
+  var $canvas = $('canvas');
 
 
+  // Pasting an image
   var $message = $('.message');
   $message.first().html('Copy your screen by pressing on the Print Screen key. Then paste (Ctrl+v) it here as an image.');
   var timeoutId;
@@ -41,19 +39,15 @@ $(document).ready(function() {
     return deferred;
   };
 
-  var $size = $('.image-size');
   function copyImageInCanvas() {
     var image = $img[0];
 
     showMessage('Copying to canvas...');
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    draw.size(canvas.width, canvas.height);
+    setCanvasSize(image.naturalWidth, image.naturalHeight);
     ctx.drawImage(image, 0, 0);
     $img.hide();
     $canvas.show();
     showMessage('Image pasted. You can paste again to replace.');
-    $size.html(image.naturalWidth + ' x ' + image.naturalHeight);
   }
 
   var IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
@@ -71,9 +65,6 @@ $(document).ready(function() {
   });
 
 
-  var rect;
-  var $selection = $('.selection');
-  var $mouse = $('.mouse-position');
   function getMousePosition(e) {
     return {
       x: Math.round(e.pageX - $content.offset().left - 5),
@@ -81,38 +72,103 @@ $(document).ready(function() {
     };
   }
 
-  function renderSelection() {
-    if (rect) 
-      $selection.html(rect.x() + ', ' + rect.y() + ' : ' + rect.width() + ' x ' + rect.height());
+
+  // Canvas resize
+  var draw, crect;
+  var canvas = $canvas[0];
+  var ctx = canvas.getContext("2d");
+
+  function renderCanvasSizeAndMousePosition(m) {
+    var $size = $('.image-size');
+    $size.html(canvas.width + ' x ' + canvas.height);
+    if (m) {
+      var $mouse = $('.mouse-position');
+      $mouse.html(m.x + ', ' + m.y);
+    }
+  }
+
+  function setCanvasSize(width, height) {
+    var image = ctx.getImageData(0, 0, Math.min(width, canvas.width), Math.min(height, canvas.height));
+    canvas.width = width;
+    canvas.height = height;
+    ctx.putImageData(image, 0, 0);
+    crect.size(width, height);
+    draw.size(width+5, height+5);
+    renderCanvasSizeAndMousePosition();
+  }
+
+  function allowCanvasResize(allow) {
+    var allowed = !!crect.remember('allowResize');
+
+    if (allow === undefined) return allowed;
+
+    if (allow === true && !allowed) {
+      crect.remember('allowResize', true)
+        .selectize({
+          points: ['rb', 'r', 'b'],
+          rotationPoint: false,
+          pointType: 'rect'
+        })
+        .resize();
+    }
+
+    if (allow === false && allowed) {
+      crect.forget('allowResize').resize('stop').selectize(false);
+    }
+  }
+
+  function initCanvas(width, height) {
+    draw = SVG('drawing').size(0, 0);
+    crect = draw.rect(canvas.width, canvas.height)
+      .addClass('canvas')
+      .on('resizing', function() {
+        draw.size(crect.width()+5, crect.height()+5);
+      })
+      .on('resizedone', function() {
+        setCanvasSize(crect.width(), crect.height());
+      });
+    setCanvasSize(width, height);
+    allowCanvasResize(true);
+  }
+  initCanvas(window.innerWidth-30, window.innerHeight-56-40-10);
+
+
+  // Selection
+  var srect;
+
+  function renderSelectionSizeAndMousePosition() {
+    var $selection = $('.selection');
+    if (srect) 
+      $selection.html(srect.x() + ', ' + srect.y() + ' : ' + srect.width() + ' x ' + srect.height());
     else
       $selection.empty();
   }
 
   function onMouseDown(e) {
     var m = getMousePosition(e);
-    if (rect) {
-      if (rect.inside(m.x, m.y)) return;
-      rect.draggable(false)
+    if (srect) {
+      if (srect.inside(m.x, m.y)) return;
+      srect.draggable(false)
         .resize('stop')
         .selectize(false)
         .remove();
-      rect = undefined;
+      srect = undefined;
+      allowCanvasResize(true);
     }
 
     var x = Math.min(Math.max(0, m.x), canvas.width);
     var y = Math.min(Math.max(0, m.y), canvas.height);
-    rect = draw.rect(0, 0);
-    rect.x(x).y(y);
-    rect.addClass('select');
-    rect.remember('start', {x: x, y: y});
-    renderSelection();
+    srect = draw.rect(0, 0);
+    srect.x(x).y(y);
+    srect.addClass('select');
+    srect.remember('start', {x: x, y: y});
+    renderSelectionSizeAndMousePosition();
   }
 
   function onMouseMove(e) {
     var m = getMousePosition(e);
-    $mouse.html(m.x + ', ' + m.y);
-    if (rect) {
-      var start = rect.remember('start');
+    if (srect) {
+      var start = srect.remember('start');
       if (start) {
         var x = Math.min(Math.max(0, Math.min(start.x, m.x)), canvas.width);
         var y = Math.min(Math.max(0, Math.min(start.y, m.y)), canvas.height);
@@ -120,22 +176,25 @@ $(document).ready(function() {
         var height = Math.max(0, Math.max(start.y, m.y) - y);
         if (x+width > canvas.width) width = canvas.width - x;
         if (y+height > canvas.height) height = canvas.height - y;
-        rect.x(x).y(y).width(width).height(height);
+        srect.x(x).y(y).width(width).height(height);
+        if (allowCanvasResize()) allowCanvasResize(false);
       }
     }
-    renderSelection();
+    renderCanvasSizeAndMousePosition(m);
+    renderSelectionSizeAndMousePosition();
   }
 
   function onMouseUp(e) {
     var m = getMousePosition(e);
-    if (rect && rect.remember('start')) {
-      if (rect.width() == 0 && rect.height() == 0) {
-        rect.remove();
-        rect = undefined;
-        renderSelection();
+    if (srect && srect.remember('start')) {
+      if (srect.width() == 0 && srect.height() == 0) {
+        srect.remove();
+        srect = undefined;
+        allowCanvasResize(true);
+        renderSelectionSizeAndMousePosition();
         return;
       }
-      rect.forget('start')
+      srect.forget('start')
         .selectize({rotationPoint: false, pointType: 'rect'})
         .resize({
           constraint: {minX: 0, minY: 0, maxX: canvas.width, maxY: canvas.height}
